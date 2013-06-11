80b4e5fa1fce3ac8ebc80103c41df0c8ecb31e88
Kristofer Krause <kris.krause@gmail.com>
2013-06-11 18:54:10 -0400

    Parse tests added

--- android/ServerMonitor/src/com/camomedia/servermonitor/tests/ParseTest.java
+++ android/ServerMonitor/src/com/camomedia/servermonitor/tests/ParseTest.java
@@ -1,22 +1,52 @@
 package com.camomedia.servermonitor.tests;
 
+import java.util.List;
+
 import android.util.Log;
 
 import com.camomedia.servermonitor.MainActivity;
+import com.parse.FindCallback;
+import com.parse.ParseACL;
+import com.parse.ParseException;
 import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
 
 public class ParseTest {
 	public static void TestAll() {
 		try {
-			Sanity_Object_Save_Test();
+			Monitor_Save_Test();
+			Monitor_Query_Test();
 		} catch (Exception ex) {
 			Log.e(MainActivity.TAG, "ParseTest", ex);
 		}
 	}
 	
-	public static void Sanity_Object_Save_Test() {	
-		ParseObject testObject = new ParseObject("TestObject");
-		testObject.put("foo", "bar");
-		testObject.saveInBackground();
+	public static void Monitor_Save_Test() {	
+		ParseObject objMon = new ParseObject("Monitoring");
+		objMon.put("ParseType", 0);
+		objMon.put("Pattern", "foo");
+		objMon.put("Tab", 0);
+		objMon.put("Url", "http://www.google.com");
+		objMon.saveInBackground();
+	}
+	
+	public static void Monitor_Query_Test() {
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("Monitoring");
+		query.whereEqualTo("Tab", 0);
+		query.findInBackground(new FindCallback<ParseObject>() {
+			@Override
+			public void done(List<ParseObject> list, ParseException e) {
+				// TODO Auto-generated method stub
+		        if (e == null) {
+		        	String pattern = (String) list.get(0).get("Pattern");
+		        	
+		            Log.d(MainActivity.TAG, "Retrieved " + list.size() + " items");
+		            Log.d(MainActivity.TAG, "With pattern  " + pattern);
+		        } else {
+		            Log.d(MainActivity.TAG, "Error: " + e.getMessage());
+		        }	
+			}
+		});
 	}
 }
