68b2cfd2b4613379aa742dca4b79cd464ca0984f
Kristofer Krause <kris.krause@gmail.com>
2013-06-13 06:23:13 -0400

    parse testing and integration

---
+++ android/ServerMonitor/src/com/camomedia/servermonitor/MonitoringItem.java
@@ -0,0 +1,51 @@
+package com.camomedia.servermonitor;
+
+public class MonitoringItem {
+	public MonitoringItem() {}
+	
+	// Parse class name.
+	public static final String PARSE_CLASS = "Monitoring";
+	
+	// Parse field names.
+	public static final String PARSE_PARSETYPE = "ParseType";
+	public static final String PARSE_TAB = "Tab";
+	public static final String PARSE_PATTERN = "Pattern";
+	public static final String PARSE_URL = "Url";
+	
+	private int _parseType;
+	private int _tab;
+	private String _pattern;
+	private String _url;
+	
+	public int get_tab() {
+		return _tab;
+	}
+
+	public void set_tab(int _tab) {
+		this._tab = _tab;
+	}
+
+	public String get_pattern() {
+		return _pattern;
+	}
+
+	public void set_pattern(String _pattern) {
+		this._pattern = _pattern;
+	}
+
+	public String get_url() {
+		return _url;
+	}
+
+	public void set_url(String _url) {
+		this._url = _url;
+	}
+	
+	public int get_parseType() {
+		return _parseType;
+	}
+
+	public void set_parseType(int _parseType) {
+		this._parseType = _parseType;
+	}
+}
--- android/ServerMonitor/src/com/camomedia/servermonitor/tests/ParseTest.java
+++ android/ServerMonitor/src/com/camomedia/servermonitor/tests/ParseTest.java
@@ -5,6 +5,7 @@ import java.util.List;
 import android.util.Log;
 
 import com.camomedia.servermonitor.MainActivity;
+import com.camomedia.servermonitor.MonitoringItem;
 import com.parse.FindCallback;
 import com.parse.ParseACL;
 import com.parse.ParseException;
@@ -23,26 +24,33 @@ public class ParseTest {
 	}
 	
 	public static void Monitor_Save_Test() {	
-		ParseObject objMon = new ParseObject("Monitoring");
-		objMon.put("ParseType", 0);
-		objMon.put("Pattern", "foo");
-		objMon.put("Tab", 0);
-		objMon.put("Url", "http://www.google.com");
+		ParseObject objMon = new ParseObject(MonitoringItem.PARSE_CLASS);
+		objMon.put(MonitoringItem.PARSE_PARSETYPE, 0);
+		objMon.put(MonitoringItem.PARSE_PATTERN, "foo");
+		objMon.put(MonitoringItem.PARSE_TAB, 0);
+		objMon.put(MonitoringItem.PARSE_URL, "http://www.google.com");
 		objMon.saveInBackground();
 	}
 	
 	public static void Monitor_Query_Test() {
 		ParseQuery<ParseObject> query = ParseQuery.getQuery("Monitoring");
-		query.whereEqualTo("Tab", 0);
+		query.whereEqualTo(MonitoringItem.PARSE_TAB, 0);
+		query.whereNotEqualTo(MonitoringItem.PARSE_PATTERN, "foo");
+		
 		query.findInBackground(new FindCallback<ParseObject>() {
 			@Override
 			public void done(List<ParseObject> list, ParseException e) {
 				// TODO Auto-generated method stub
 		        if (e == null) {
-		        	String pattern = (String) list.get(0).get("Pattern");
-		        	
-		            Log.d(MainActivity.TAG, "Retrieved " + list.size() + " items");
-		            Log.d(MainActivity.TAG, "With pattern  " + pattern);
+		        	for (int i = 0; i < list.size(); i++) {
+			        	MonitoringItem item = new MonitoringItem();
+			        	
+			        	item.set_pattern((String) list.get(i).get(MonitoringItem.PARSE_PATTERN));
+			        	item.set_url((String) list.get(i).get(MonitoringItem.PARSE_URL));
+			        	
+			            Log.d(MainActivity.TAG, "Retrieved " + item.get_url());
+			            Log.d(MainActivity.TAG, "With pattern  " + item.get_pattern());
+		        	}
 		        } else {
 		            Log.d(MainActivity.TAG, "Error: " + e.getMessage());
 		        }	
